8b14cc9659ccea95215e8bf668dc64726078f174
svenjadunger <svenjadunger@web.de>
2024-06-23 17:54:45 +0200

    add Vampire class with name,age,blood group attributes

--- model/Vampire.java
+++ model/Vampire.java
@@ -1,5 +1,15 @@
 package model;
 
 public class Vampire {
+    private String name;
+    private int alter;
+    private String blutgruppe;
 
+    public Vampire(String name, int alter, String blutgruppe) {
+        this.name = name;
+        this.alter = alter;
+        this.blutgruppe = blutgruppe;
+    }
+
+    
 }
