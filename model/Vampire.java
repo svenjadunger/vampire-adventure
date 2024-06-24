5e403fc73aa221d342115ee7fa542431b8561e59
svenjadunger <svenjadunger@web.de>
2024-06-24 13:45:46 +0200

    add operations to Vampire.java

--- model/Vampire.java
+++ model/Vampire.java
@@ -2,13 +2,18 @@ package model;
 
 public class Vampire {
     private String name;
-    private int alter;
-    private String blutgruppe;
+    private int age; 
+    private int grandness;
+    private int hunger;
+    private int energy;
+    private boolean finallyDead;
+    private boolean doublePower;
+    private boolean transparency;
 
-    public Vampire(String name, int alter, String blutgruppe) {
+    // Konstruktor
+    public Vampire(String name, int age) {
         this.name = name;
-        this.alter = alter;
-        this.blutgruppe = blutgruppe;
+        this.age = age;
     }
 
     // Getter und Setter
@@ -20,21 +25,95 @@ public class Vampire {
         this.name = name;
     }
 
-    public int getAlter() {
-        return alter;
+    public int getAge() {
+        return age;
     }
 
-    public void setAlter(int alter) {
-        this.alter = alter;
+    public void setAge(int age) {
+        this.age = age;
     }
 
-    public String getBlutgruppe() {
-        return blutgruppe;
+    public int getGrandness() {
+        return grandness;
     }
 
-    public void setBlutgruppe(String blutgruppe) {
-        this.blutgruppe = blutgruppe;
+    public void setGrandness(int grandness) {
+        this.grandness = grandness;
     }
 
-   
+    public int getHunger() {
+        return hunger;
+    }
+
+    public void setHunger(int hunger) {
+        this.hunger = hunger;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(int energy) {
+        this.energy = energy;
+    }
+
+    public boolean isFinallyDead() {
+        return finallyDead;
+    }
+
+    public void setFinallyDead(boolean finallyDead) {
+        this.finallyDead = finallyDead;
+    }
+
+    public boolean isDoublePower() {
+        return doublePower;
+    }
+
+    public void setDoublePower(boolean doublePower) {
+        this.doublePower = doublePower;
+    }
+
+    public boolean isTransparency() {
+        return transparency;
+    }
+
+    public void setTransparency(boolean transparency) {
+        this.transparency = transparency;
+    }
+
+    // Methoden
+    public void attackHuman(Human human) {
+    }
+
+    public void drinkBlood(double amount) {
+    }
+
+    public void takeDamage(int damage) {
+        if (this.transparency) {
+            
+            if (Math.random() > 0.5) {
+                return;
+            }
+        }
+        this.energy -= damage;
+        if (this.energy <= 0) {
+            this.finallyDead = true;
+        }
+    }
+
+    public void attack(VampireHunter vh) {
+    }
+
+    public boolean flee() {
+        return Math.random() <= 0.6;
+    }
+
+    @Override
+    public String toString() {
+        return "Vampir Name: " + name + "\nAlter: " + age +
+               "\nMächtigkeit: " + grandness + "\nHunger: " + hunger + 
+               "\nEnergie: " + energy + "\nFähigkeiten:\n  - Double Power: " + 
+               (doublePower ? "aktiviert" : "deaktiviert") +
+               "\n  - Transparency: " + (transparency ? "aktiviert" : "deaktiviert");
+    }
 }
