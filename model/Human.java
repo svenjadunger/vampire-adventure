7cf6e65c5092fde0c39f50d134a310e8d415948d
TalaKaroum <Tala.Karoum@Student.HTW-Berlin>
2024-06-24 14:15:19 +0200

    add getter & setter in HUman.java

--- model/Human.java
+++ model/Human.java
@@ -1,4 +1,29 @@
 package model;
 
 public class Human {
+    private int amountOfBlood;
+
+//Konstruktor
+public Human (int amountOfBlood) {
+    this.amountOfBlood = amountOfBlood;
+}
+
+// Getter & Setter
+public int getAmountOfBlood () {
+    return amountOfBlood;
+}
+
+public void setAmountOfBlood (int amountOfBlood) {
+    this.amountOfBlood = amountOfBlood;
+}
+
+//Methode
+public boolean flee () {
+    return Math.random() <= 0.2;
+}
+
+public void looseBlood(double amount) {
+    
+}
+
 }
