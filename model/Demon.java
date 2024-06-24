83a0f8d7f9148ec32e89920de24334ea6a8c6408
TalaKaroum <Tala.Karoum@Student.HTW-Berlin>
2024-06-24 14:23:50 +0200

    add getter & setter Demon.java

--- model/Demon.java
+++ model/Demon.java
@@ -1,4 +1,34 @@
 package model;
 
 public class Demon {
+    private String name;
+    private String vampireAbility;
+
+    //Konstruktor
+public Demon (String name, String vamipreAbility) {
+    this.name = name;
+    this.vampireAbility = vamipreAbility;
+}
+
+// Getter & Setter
+public String getName () {
+    return name;
+}
+
+public void setName (String name) {
+    this.name = name;
+}
+public String getVampireAbility () {
+    return vampireAbility;
+}
+
+public void setVampireAbility (String vamipreAbility) {
+    this.vampireAbility = vamipreAbility;
+}
+
+//Methode
+public void presentTask () {
+    
+}
+
 }
