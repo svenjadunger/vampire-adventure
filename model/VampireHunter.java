bb06833b5b4ee53e28a94511ad627a065e20f71b
TalaKaroum <Tala.Karoum@Student.HTW-Berlin>
2024-06-24 14:31:47 +0200

    getter & setter to vampireHunter.java

--- model/VampireHunter.java
+++ model/VampireHunter.java
@@ -1,4 +1,42 @@
 package model;
 
 public class VampireHunter {
+    private String name;
+    private int energy;
+
+       //Konstruktor
+public VampireHunter (String name, int energy) {
+    this.name = name;
+    this.energy = energy;
+}
+// Getter & Setter
+public String getName () {
+    return name;
+}
+
+public void setName (String name) {
+    this.name = name;
+}
+
+public int getEnergy () {
+    return energy;
+}
+public void setEnergy (int energy) {
+    this.energy = energy;
+}
+
+//Methoden
+public void attack(Vampire vampire) {
+
+}
+public void takeDemage(int amount) {
+
+}
+public boolean flee() {
+    return flee();
+}
+public void alive() {
+
+}
+
 }
